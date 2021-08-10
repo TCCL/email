4b84b1b6c042274fda0403ddc7cee6642ebb254f
Roger Gee <roger.gee@tulsalibrary.org>
2021-08-10 16:44:34 -0500

    Add mime-header utf-8 encoding to Mailer

--- lib/Mailer.php
+++ lib/Mailer.php
@@ -9,12 +9,40 @@
 namespace TCCL\Email;
 
 class Mailer {
+    /**
+     * Encodes an email address and (optionally) recipient name for use in an
+     * email header.
+     *
+     * @param string $emailAddress
+     * @param string $name
+     *
+     * @return string
+     */
+    public static function encodeEmail($emailAddress,$name = null) {
+        mb_internal_encoding("UTF-8");
+
+        if (is_null($name)) {
+            if (preg_match('/^(.*)<(.*)>$/',$emailAddress,$matches)) {
+                list($name,$emailAddress) = array_slice($matches,1);
+            }
+        }
+
+        if (!empty($name)) {
+            $eUserName = mb_encode_mimeheader($name,'UTF-8','Q');
+            $eUserName = str_replace('"','\"',$eUserName);
+            $eUserName = "\"$eUserName\"";
+            return "$eUserName <$emailAddress>";
+        }
+
+        return $emailAddress;
+    }
+
     private $generator;
 
     /**
      * Creates a new Mailer instance.
      *
-     * @param EmailGenerator $generator
+     * @param \TCCL\Email\EmailGenerator $generator
      *  The generator that will create the email message.
      */
     public function __construct(EmailGenerator $generator) {
@@ -22,11 +50,41 @@ class Mailer {
     }
 
     /**
+     * Calls mail() to send the email.
+     *
+     * @param string $subject
+     * @param string $from
+     * @param string $toAddress
+     * @param string $toUser
+     *
+     * @return bool
+     */
+    public function sendMail($subject,$from,$toAddress,$toUser = null) {
+        mb_internal_encoding('UTF-8');
+
+        $content = $this->generator->getContent();
+        $headers = $this->generator->getHeaders();
+
+        $headers['From'] = mb_encode_mimeheader($from,'UTF-8','Q');
+        $headers['MIME-Version'] = 1.0;
+
+        $h = '';
+        foreach ($headers as $key => $value) {
+            $h .= "$key: $value\r\n";
+        }
+
+        $to = self::encodeEmail($toAddress,$toUser);
+        $subjectEncoded = mb_encode_mimeheader($subject,'UTF-8','Q');
+
+        return mail($to,$subjectEncoded,$content,$h);
+    }
+
+    /**
      * Calls mail() to send the email message.
      *
-     * @param string $emailFrom
+     * @param string $from
      *  The sender email address.
-     * @param string $emailTo
+     * @param string $to
      *  The recipient email address.
      * @param string $subject
      *  The email address subject.
@@ -34,17 +92,23 @@ class Mailer {
      * @return bool
      *  The return value is that from the underlying call to mail().
      */
-    public function send($emailFrom,$emailTo,$subject) {
+    public function send($from,$to,$subject) {
+        mb_internal_encoding('UTF-8');
+
         $content = $this->generator->getContent();
         $headers = $this->generator->getHeaders();
 
-        $headers['From'] = $emailFrom;
+        $headers['From'] = mb_encode_mimeheader($from,'UTF-8','Q');
         $headers['MIME-Version'] = 1.0;
+
         $h = '';
         foreach ($headers as $key => $value) {
             $h .= "$key: $value\r\n";
         }
 
-        return mail($emailTo,$subject,$content,$h);
+        $toEncoded = self::encodeEmail($to);
+        $subjectEncoded = mb_encode_mimeheader($subject,'UTF-8','Q');
+
+        return mail($toEncoded,$subjectEncoded,$content,$h);
     }
 }
